b360eb0d79c560736e395d0b9dae13709e2543f6
mahmoudahmedd <mahmoudaahmedd@gmail.com>
2018-04-27 04:20:45 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include  "image.h"
+
+using namespace std;
+
+int main()
+{
+
+	image a("test.bmp");
+	a.darken_image(.5);
+	a.lighten_image(.3);
+	a.save_image("new.bmp");
+	
+	return 0;
+}
+	
